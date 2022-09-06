8c7fb43835862e73dfd179308c30df74848788c1
guilherme <guilherme.bassi.sp@gmail.com>
2022-09-06 22:46:09 +0930

    created project

---
+++ example/english_numerals_example.dart
@@ -0,0 +1,6 @@
+import 'package:english_numerals/english_numerals.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/english_numerals.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library english_numerals;
+
+export 'src/english_numerals_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
